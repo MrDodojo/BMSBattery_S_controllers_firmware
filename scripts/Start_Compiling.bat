1bf8512049de8d00f0c41c1c0929e73d5bc26763
LosVast <47711280+MrDodojo@users.noreply.github.com>
2024-04-08 13:27:09 +0200

    fix windows building
    Made changes in SDCC launch settings, updated makefile to be up to Linux

--- scripts/Start_Compiling.bat
+++ scripts/Start_Compiling.bat
@@ -1,12 +1,15 @@
 PATH = %PATH%;C:\Program Files\STMicroelectronics\st_toolset\stvp;C:\Program Files (x86)\STMicroelectronics\st_toolset\stvp;C:\SDCC\usr\local\bin
 REM ;%~dp0tools\cygwin\bin
 cd %~dp0
-del main.hex
 sdcc --version
-make -f Makefile_windows clean
-make -f Makefile_windows
+::Move location to project folder
+cd ..
+del main.hex
+del main.ihx
+tooling\make -f Makefile_windows clean
+tooling\make -f Makefile_windows
 ren main.ihx main.hex
-make -f Makefile_windows clean
+tooling\make -f Makefile_windows clean
 
 STVP_CmdLine -BoardName=ST-LINK -ProgMode=SWIM -Port=USB -Device=STM8S105x6 -FileProg=main.hex -verbose -no_loop
 
